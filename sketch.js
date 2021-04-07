4ac18c29ccfdd8f4b87f2f6ac33f1f9a0575d974
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-04-07 07:08:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,11 +5,15 @@ var s;
 var hours;
 
 function setup() {
-    createCanvas(600,600);
+    createCanvas(400,400);
+    angleMode(DEGREES);
 }
 
 function draw() {
     background(0);
+
+    translate(200,200);
+    rotate(-90);
 
     h = hour();
     m = minute();
@@ -17,56 +21,51 @@ function draw() {
     
     sAngle = map(s,0,60,0,360);
     mAngle = map(m,0,60,0,360);
-    hAngle = map(h,0,12,0,360);
+    hAngle = map(h%12,0,12,0,360);
 
     push();
-      translate(0,0);
-      angleMode(DEGREES);
       rotate(sAngle);
-      stroke(255,0,0);
+      stroke(255,140,0);
       strokeWeight(7);
-      line(0,0,0,-100);
+      line(0,0,100,0);
     pop();
 
     push();
-      angleMode(RADIANS);
-      stroke(255,0,0);
+      stroke(255,140,0);
       strokeWeight(7);
       noFill();
-      arc(0,0,270,270,PI+PI/2,(PI*(sAngle/180))-PI/2);
+      arc(0,0,270,270,0,sAngle);
     pop();
 
     push();
-      translate(0,0);
-      angleMode(DEGREES);
       rotate(mAngle);
-      stroke(0,255,0);
+      stroke(255,255,255);
       strokeWeight(7);
-      line(0,0,0,-50);
+      line(0,0,70,0);
     pop();
 
     push();
-      angleMode(RADIANS);
-      stroke(0,255,0);
+      stroke(255,255,255);
       strokeWeight(7);
       noFill();
-      arc(0,0,255,255,PI+PI/2,(PI*(mAngle/180))-PI/2);
+      arc(0,0,255,255,0,mAngle);
     pop();
 
     push();
-      translate(0,0);
-      angleMode(DEGREES);
       rotate(hAngle);
-      stroke(0,0,255);
+      stroke(0,255,0);
       strokeWeight(7);
-      line(0,0,0,-25);
+      line(0,0,45,0);
     pop();
 
     push();
-      angleMode(RADIANS);
-      stroke(0,0,255);
+      stroke(0,255,0);
       strokeWeight(7);
       noFill();
-      arc(0,0,240,240,PI+PI/2,(PI*(hAngle/180))-PI/2);
+      arc(0,0,240,240,0,hAngle);
     pop();
+   
+    strokeWeight(8);
+    stroke(255,0,255);
+    point(0,0);
 }
