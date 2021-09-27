c6be72911719c64fb987af769e3d211394da7bcf
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-09-27 13:16:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,14 +5,14 @@ var s;
 var hours;
 
 function setup() {
-    createCanvas(400,400);
+    createCanvas(windowWidth,windowHeight);
     angleMode(DEGREES);
 }
 
 function draw() {
     background(0);
 
-    translate(200,200);
+    translate(width/2,height/2);
     rotate(-90);
 
     h = hour();
@@ -27,42 +27,42 @@ function draw() {
       rotate(sAngle);
       stroke(255,140,0);
       strokeWeight(7);
-      line(0,0,100,0);
+      line(0,0,300,0);
     pop();
 
     push();
       stroke(255,140,0);
       strokeWeight(7);
       noFill();
-      arc(0,0,270,270,0,sAngle);
+      arc(0,0,710,710,0,sAngle);
     pop();
 
     push();
       rotate(mAngle);
       stroke(255,255,255);
       strokeWeight(7);
-      line(0,0,70,0);
+      line(0,0,210,0);
     pop();
 
     push();
       stroke(255,255,255);
       strokeWeight(7);
       noFill();
-      arc(0,0,255,255,0,mAngle);
+      arc(0,0,695,695,0,mAngle);
     pop();
 
     push();
       rotate(hAngle);
       stroke(0,255,0);
       strokeWeight(7);
-      line(0,0,45,0);
+      line(0,0,135,0);
     pop();
 
     push();
       stroke(0,255,0);
       strokeWeight(7);
       noFill();
-      arc(0,0,240,240,0,hAngle);
+      arc(0,0,680,680,0,hAngle);
     pop();
    
     strokeWeight(8);
